45ab345c7712fc27e062be37a4f3f082631ec640
fdhlnadli <fadhlansatrio15@gmail.com>
2024-11-24 11:30:37 +0700

    fix: fixing image in the layout and tha page anime list
    Styling component dan modyfing halaman utama

--- src/app/components/Utilities/Navbar/index.js
+++ src/app/components/Utilities/Navbar/index.js
@@ -3,8 +3,10 @@ import Link from "next/link";
 const Navbar = () => {
    return (
       <header className="bg-gray-600">
-         <div>
-            <Link href="/">NextAnimeList</Link>
+         <div className="flex md:flex-row flex-col justify-between p-4 gap 2">
+            <Link href="/" className="text-2xl font-bold">
+               NextAnimeList
+            </Link>
             <input type="placeholder" placeholder="Search Anime" />
          </div>
       </header>
--- src/app/page.js
+++ src/app/page.js
@@ -1,14 +1,23 @@
+import Link from "next/link";
 import AnimeList from "./components/AnimeList";
 
 export const Home = async () => {
    const response = await fetch(
-      `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime`
+      `${process.env.NEXT_PUBLIC_API_BASE_URL}/top/anime?limit=8`
    );
    const anime = await response.json();
    return (
       <div>
-         <h1>NextAnimeList</h1>
-         <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4">
+         <div className="flex justify-between items-center p-4">
+            <h1 className="text-xl font-bold">Paling Populer</h1>
+            <Link
+               href="/populer"
+               className="md:text-xl text-lg underline hover:text-indigo-500 transition-all"
+            >
+               Lihat Semua
+            </Link>
+         </div>
+         <div className="grid md:grid-cols-4 sm:grid-cols-3 grid-cols-2 gap-4 px-4">
             {anime.data.map((data) => {
                return (
                   <div key={data.mal_id}>
