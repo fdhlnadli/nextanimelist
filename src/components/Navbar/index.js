7f8dce9bb0d30dc532a09e4bed04e04785d38873
fdhlnadli <fadhlansatrio15@gmail.com>
2024-12-27 19:39:36 +0700

    fix: menambah style classname input search nav

--- src/components/Navbar/index.js
+++ src/components/Navbar/index.js
@@ -2,12 +2,16 @@ import Link from "next/link";
 
 const Navbar = () => {
    return (
-      <header className="bg-gray-600">
+      <header className="bg-color-secondary">
          <div className="flex md:flex-row flex-col justify-between p-4 gap 2">
             <Link href="/" className="text-2xl font-bold">
                NextAnimeList
             </Link>
-            <input type="placeholder" placeholder="Search Anime" />
+            <input
+               type="placeholder"
+               placeholder="Search Anime"
+               className="py-1 px-2 rounded-md"
+            />
          </div>
       </header>
    );
